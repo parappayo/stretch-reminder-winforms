e6c6dc535da18c26d127256abcfc288f05400d58
Jason Estey <esteyj@yahoo.com>
2021-06-14 20:44:14 -0700

    initial commit, tray app with notification

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stretch-reminder-winforms
-Minimal system tray app that shows periodic notifications
+
+Minimal system tray app that shows periodic notifications.
+
+Built for .NET 5 in C# using Visual Studio Community.
---
+++ StretchReminder/TrayApplicationContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StretchReminder
+{
+    public class TrayApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public TrayApplicationContext()
+        {
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = SystemIcons.Information,
+                Text = ConfigurationManager.AppSettings["notificationText"],
+                BalloonTipText = ConfigurationManager.AppSettings["notificationBalloonTipText"],
+                Visible = true
+            };
+
+            var timerPeriod = int.Parse(ConfigurationManager.AppSettings["notificationPeriod"]) * 1000;
+            timerPeriod = Math.Min(timerPeriod, 20000);
+
+            var timer = new System.Timers.Timer(timerPeriod);
+            timer.Elapsed += ShowNotification;
+            timer.Enabled = true;
+        }
+
+        private void ShowNotification(object sender, EventArgs eventArgs)
+        {
+            var timerDuration = int.Parse(ConfigurationManager.AppSettings["notificationDuration"]) * 1000;
+            _trayIcon.ShowBalloonTip(timerDuration);
+        }
+    }
+}
